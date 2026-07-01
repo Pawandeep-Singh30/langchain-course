42c4ec88aa371b363746ff1a141dec7b8645858b
Singh Pawandeep <Singh.Pawandeep@latticesemi.com>
2026-07-01 09:42:10 +0800

    WIP: current agent setup

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
+from typing import List
+from pydantic import BaseModel, Field 
 from dotenv import load_dotenv
-
 load_dotenv()
+from langchain.agents import create_agent
+from langchain_core.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_ollama import ChatOllama
+from langchain_tavily import TavilySearch
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+    url:str = Field(description="The url of the source")
+class AgentResponse(BaseModel):
+    """Schema for agent reponse with answer and sources"""
+    answer:str = Field(description="The agent's answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
 
-
+llm = ChatOllama(temperature=0, model="llama3.2")
+tools = [TavilySearch()]
+agent = create_agent(llm, tools=tools, response_format=AgentResponse)
 def main():
-    print("Hello from langchain-course!")
-
-
+    print("Hello from LangChain!")
+    result = agent.invoke(
+        {
+            "messages": HumanMessage(
+                content="search for 3 job postings for an ai engineer using langchain in penang on linked in and list their details"
+                )
+            }
+    )
+    print(result)
 if __name__ == "__main__":
     main()
